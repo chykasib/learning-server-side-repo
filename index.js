91ba1c449d3401d56d2e4ba5b1132b798fcc7e82
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2022-10-25 10:48:45 +0600

    add data

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = process.env.port || 5000;
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('hello courses!')
+const courses = require('./data/Courses.json');
+const coursesDetails = require('./data/CoursesDetails.json');
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = coursesDetails.find(details => details.id == id)
+    res.send(course)
+
+})
+
+app.get('/CoursesDetails', (req, res) => {
+    res.send(coursesDetails)
+})
+
+app.get('/CoursesDetails/:id', (req, res) => {
+    const id = req.params.id;
+    const details = coursesDetails.find(details => details.id == id);
+    res.send(details)
+})
+app.get('/', (req, res) => {
+    res.send('hello courses...')
+})
 app.listen(port, () => {
     console.log(`cse courses coming soon!! ${port}`)
 })
